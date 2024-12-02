d48eea29bc100d3ea2b4def2d4a0634d3543b649
WSBHRDAS <chelnea@gmail.com>
2024-12-02 09:31:38 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,8 +1,3 @@
-# LIST_ = {'od' : '1a', 'dv': '2b', 'tr': '33'}
-#
-#
-# for i, k in LIST_.items():
-#     print (i, k)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 num_ch = 0  # числитель
@@ -32,28 +27,5 @@ for i in range(len(numbers)) :
         else :
             primes.append(num_ch)
 
-        #print (num_ch, "  ", num_zn, "  Neprostoe: ", num_neprostoe)
-
-             #   if
-
-            # num_zn = numbers [k]
-            #
-#
-#                     if num_ch % num_zn != 0 :
-#                         print("Найдено простое число:", num_ch)
-#                         num_neprostoe = False  # найдено непростое число
-#
-#                     else:
-#                         print("Найдено не простое (составное число):", num_ch)
-#                         not_primes.append(num_ch)
-#                         num_neprostoe = True  # найдено непростое число
-#                         break
-#                     print(num_ch, " num_zn: ", num_zn)
-#                     primes.append(num_ch)
-#                     break
-#                     num_neprostoe = False
-#
 print ("Primes: ", primes)
 print ("Not_primes: ",not_primes)
-#
-#
